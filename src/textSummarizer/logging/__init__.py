1ff887ca618347b34b1b36f1bc081af117fe94fe
Sajjad Amjad <sajjadamjad0123@gmail.com>
2023-06-22 08:30:35 +0500

    requirements & installation done

---
+++ src/textSummarizer/logging/__init__.py
@@ -0,0 +1,18 @@
+import os, sys, logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=log_dir,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("textSummarizerLogger")
